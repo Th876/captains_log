eddfcdccea3aaa2b727edcdec7864a3894942387
TH876 <tayhiggins14@gmail.com>
2023-05-16 19:19:03 -0400

    add index route minus link to create page

---
+++ models/logs.js
@@ -0,0 +1,31 @@
+// import
+const mongoose = require("mongoose"); // require mongoose
+
+// Configs
+const Schema = mongoose.Schema; // create a shorthand for the mongoose Schema 
+constructor
+const model = mongoose.model // shorthand for model function
+
+
+const logsSchema = new Schema(
+    {
+      title: {
+        type: String,
+      },
+  
+      entry: {
+        type: String,
+      }, 
+
+      shipIsBroken: {
+        type: Boolean, 
+        default: true,
+      }
+    }
+  );
+  
+  
+  const Log = model("Log", logsSchema);
+  
+  //make this exportable to be accessed in `app.js`. Is the last line of code in this file
+  module.exports = Log;
--- server.js
+++ server.js
@@ -3,11 +3,33 @@ const express = require('express');
 // require body parser
 const bodyParser = require('body-parser');
 
+const logs = require('./models/logs');
+
 // set express()to a variable
 const app = express();
 
 //set a variable of port to 3000
 const port = 3000;
+
+// require mongoose
+const mongoose = require('mongoose');
+
+// Add dotenv
+require('dotenv').config();
+
+// upgrade your code to create your log in MongoDB
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser:
+    true, useUnifiedTopology: true });
+    mongoose.connection.once('open', ()=> {
+        console.log('connected to mongo');
+    });
+
+//MIDDLEWARE here. ALWAYS PLACE MIDDLEWARE BEFORE ROUTES
+app.use((req, res, next) => {
+    console.log('I run for all routes');
+    next();
+});
+
 
 // View engines
 app.set('view engine', 'jsx');
@@ -27,8 +49,25 @@ const jsonParser = bodyParser.json()
 // app.use(bodyParser.json())
 
 // Index : Show all the things!
+app.get('/logs', (req, res) => {
+    logs.find({}, (error, allLogs) => {
+        logs: allLogs
+        res.render('views/Index');
+    });
+    
+});
+
+/*
+app.get('/fruits', (req, res)=>{
+    Fruit.find({}, (error, allFruits)=>{
+        res.render('fruits/Index', {
+            fruits: allFruits
+        });
+    });
+});
+*/ 
 // New : An empty form for a new thing
-app.get('/new', (req, res) => {
+app.get('/logs/new', (req, res) => {
         res.render('New');
 });
 // Delete : Get rid of this particular thing!
@@ -44,8 +83,11 @@ app.post('/logs', urlencodedParser, function (req, res) {
     } else {
         req.body.shipIsBroken === false;
     }
-
-    res.send(req.body);
+    // Must be the last line of code to 
+    // res.send(req.body);
+    logs.create(req.body, (error, createdLog)=>{
+        res.redirect('/Show');
+    });
 
 });
 
