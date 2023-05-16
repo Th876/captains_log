7945291117a04ce48d3bbe0dfa76933e45e90309
TH876 <tayhiggins14@gmail.com>
2023-05-16 14:57:36 -0400

    init

---
+++ server.js
@@ -0,0 +1,7 @@
+// Index : Show all the things!
+// New : An empty form for a new thing
+// Delete : Get rid of this particular thing!
+// Update : Update this specific thing with this updated form
+// Create : Make a new thing with this filled out form
+// Edit : A prefilled form to update a specific thing
+// Show : Show me this one thing!
