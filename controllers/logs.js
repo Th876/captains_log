060c4e34838d0e320b9bd779cb308e6f03cfe41e
TH876 <tayhiggins14@gmail.com>
2023-05-17 20:16:32 -0400

    started refactoring code

---
+++ controllers/logs.js
@@ -0,0 +1,78 @@
+const express = require('express');
+const router = express.Router();
+const Log = require('../models/logs');
+
+// Index : Show all the things!
+router.get('/', (req, res) => {
+    Log.find({}, (error, allLog) => {
+        res.render('Index', {logs: allLog} ); 
+    });
+});
+
+// New : An empty form for a new thing
+router.get('/new', (req, res) => {
+        res.render('New');
+});
+// Delete : Get rid of this particular thing! 
+
+router.delete('/:id', (req, res)=>{
+    Log.findByIdAndRemove(req.params.id, (err, data)=>{
+        res.redirect('/logs');//redirect back to logs index
+    });
+});
+
+// Update : Update this specific thing with this updated form
+router.put('/:id', (req, res)=>{
+    console.log(req.body);
+    if(req.body.shipIsBroken === 'on'){
+        req.body.shipIsBroken = true;
+    } else {
+        req.body.shipIsBroken = false;
+    }
+    Log.findByIdAndUpdate(req.params.id, req.body, (err, 
+updatedLog)=>{
+       console.log(updatedLog)
+        res.redirect(`/logs/${req.params.id}`);
+    });
+});
+
+
+// Create : Make a new thing with this filled out form
+router.post('/', function (req, res) {
+    // res.send('received');
+    console.log(req.body);
+    if(req.body.shipIsBroken === 'on'){
+        req.body.shipIsBroken === true;
+    } else {
+        req.body.shipIsBroken === false;
+    }
+    // Must be the last line of code to 
+    // res.send(req.body);
+    Log.create(req.body, (err, createdLog)=>{
+        res.redirect('/logs');
+    });
+});
+
+// Edit : A prefilled form to update a specific thing
+router.get('/:id/edit', (req, res)=>{
+    Log.findById(req.params.id, (err, foundLog)=>{ //find the log
+      if(!err){
+        res.render('Edit',
+    		{logs: foundLog //pass in the found log so we can prefill the form
+    		}
+    	);
+    } else {
+      res.send({ msg: err.message })
+    }
+    });
+});
+
+
+// Show : Show me this one thing!
+router.get('/:id', (req, res)=>{
+    Log.findById(req.params.id, (err, foundLog)=>{
+        res.render('Show', {log:foundLog})
+    });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -17,9 +17,12 @@ const port = 3000;
 
 // require mongoose
 const mongoose = require('mongoose');
+// mongoose.set("strictQuery", false);
 
 // Add dotenv
 require('dotenv').config();
+
+const logsControllers = require('./controllers/logs');
 
 // upgrade your code to create your log in MongoDB
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser:
@@ -42,90 +45,9 @@ app.set('view engine', 'jsx');
 app.engine('jsx', require('jsx-view-engine').createEngine
 ());
 
-// Middleware
-// use and configure body-parser
-// manage url encoded data in all routes (app.use)
-// parse application/x-www-form-urlencoded
-// const urlencodedParser = bodyParser.urlencoded({ extended: false })
-// app.use(bodyParser.urlencoded({ extended: false }))
+// Routes
+app.use('/logs', logsControllers);
 
-// manage json data in all routes (app.use)
-// parse application/json
-// const jsonParser = bodyParser.json()
-// app.use(bodyParser.json())
-
-// Index : Show all the things!
-app.get('/logs', (req, res) => {
-    Log.find({}, (error, allLog) => {
-        res.render('Index', {logs: allLog} ); 
-    });
-});
-
-// New : An empty form for a new thing
-app.get('/logs/new', (req, res) => {
-        res.render('New');
-});
-// Delete : Get rid of this particular thing! 
-
-app.delete('/logs/:id', (req, res)=>{
-    Log.findByIdAndRemove(req.params.id, (err, data)=>{
-        res.redirect('/logs');//redirect back to logs index
-    });
-});
-
-// Update : Update this specific thing with this updated form
-app.put('/logs/:id', (req, res)=>{
-    console.log(req.body);
-    if(req.body.shipIsBroken === 'on'){
-        req.body.shipIsBroken = true;
-    } else {
-        req.body.shipIsBroken = false;
-    }
-    Log.findByIdAndUpdate(req.params.id, req.body, (err, 
-updatedLog)=>{
-       console.log(updatedLog)
-        res.redirect(`/logs/${req.params.id}`);
-    });
-});
-
-
-// Create : Make a new thing with this filled out form
-app.post('/logs', function (req, res) {
-    // res.send('received');
-    console.log(req.body);
-    if(req.body.shipIsBroken === 'on'){
-        req.body.shipIsBroken === true;
-    } else {
-        req.body.shipIsBroken === false;
-    }
-    // Must be the last line of code to 
-    // res.send(req.body);
-    Log.create(req.body, (err, createdLog)=>{
-        res.redirect('/logs');
-    });
-});
-
-// Edit : A prefilled form to update a specific thing
-app.get('/logs/:id/edit', (req, res)=>{
-    Log.findById(req.params.id, (err, foundLog)=>{ //find the log
-      if(!err){
-        res.render('Edit',
-    		{logs: foundLog //pass in the found log so we can prefill the form
-    		}
-    	);
-    } else {
-      res.send({ msg: err.message })
-    }
-    });
-});
-
-
-// Show : Show me this one thing!
-app.get('/logs/:id', (req, res)=>{
-    Log.findById(req.params.id, (err, foundLog)=>{
-        res.render('Show', {log:foundLog})
-    });
-});
 
 // Server listening
 app.listen(port, () => {
